ef2559f117fa2ef85c826d3997b5dcd0a127f14e
ShivaleelaPatil <44635994+ShivaleelaPatil@users.noreply.github.com>
2018-11-21 17:50:23 +0530

    assesement

---
+++ Assignment/Assessment/Assessment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment
+{
+  public  class Employee
+    {
+        public int Salary;
+        public int Bonus=1000;
+
+    }
+    public class SalesMan:Employee
+    {
+        public int TotalSalary;
+        
+        public  SalesMan()
+        {
+            
+            TotalSalary = Salary + ((10 / 100) * Salary);
+        }
+
+    }
+    public class Manager:Employee
+    {
+      public  int ManagerSalary;
+        public Manager()
+        {
+            ManagerSalary = Salary + ((10 / 100) * Salary);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SalesMan salesMan = new SalesMan();
+            salesMan.Salary = 10000;
+            Manager manager = new Manager();
+            manager.Salary = 10000;
+            Console.WriteLine("salesman Salary" + salesMan.TotalSalary);
+
+
+        }
+    }
+}
+
